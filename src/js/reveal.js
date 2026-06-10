1e4b3531b7ea072acc76dbdb45b85156484fab75
gbreye <gbrdsmain@gmail.com>
2026-06-10 09:07:42 -0300

    added animations to carrousel and put a more natural text

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -1,7 +1,23 @@
 
 const rightHero = document.querySelector('.right-hero');
 const leftHero = document.querySelector('.left-hero');
+const carrousel = document.querySelector('.carrousel');
 
+for(let i = 0; i < carrousel.children.length; i++) {
+    gsap.from(carrousel.children[i], {
+        scrollTrigger: {
+            trigger: carrousel.children[i],
+            start: "top 80%", 
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+        }, 
+        opacity: 0,
+        y: 50,
+        delay: 0.5,
+        duration: 1,
+        ease: "power2.out"
+    });
+}
 gsap.from(rightHero, {
     scrollTrigger: {
         trigger: rightHero,
