bf49d4a6e94c8edc7fb2799f254a11b68f873348
gbreye <gbrdsmain@gmail.com>
2026-06-15 12:49:33 -0300

    added more sytax

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -1,22 +1,36 @@
-
+const mainleft = document.querySelector('.main-left');
+const mainright = document.querySelector('.main-right');
 const rightHero = document.querySelector('.right-hero');
 const leftHero = document.querySelector('.left-hero');
 const carrousel = document.querySelector('.carrousel');
 
-for(let i = 0; i < carrousel.children.length; i++) {
-    gsap.from(carrousel.children[i], {
-        scrollTrigger: {
-            trigger: carrousel.children[i],
-            start: "top 80%", 
-            end: "bottom 20%",
-            toggleActions: "play none none reverse",
-        }, 
-        opacity: 0,
-        y: 50,
-        duration: 1,
-        ease: "power2.out"
-    });
-}
+gsap.fromTo(mainleft, {
+    filter: "blur(10px)",
+    opacity: 0,
+    y: 50,
+}, {
+    filter: "blur(0px)",
+    opacity: 1,
+    y: 0,
+})
+
+
+gsap.from(carrousel, {
+    scrollTrigger: {
+        trigger: carrousel,
+        start: "top 80%",
+        end: "bottom 20%",
+        toggleActions: "play none none reverse",
+    }, 
+    filter: "blur(10px)",
+    duration: 1,
+    opacity: 0,
+    y: 50,
+    delay: 0.5,
+    duration: 1,
+    ease: "expo"
+});
+
 gsap.from(rightHero, {
     scrollTrigger: {
         trigger: rightHero,
