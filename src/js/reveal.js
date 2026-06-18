4d2baa03b0e4fb513d1bf034caed37a6b3e370fb
gbreye <gbrdsmain@gmail.com>
2026-06-18 09:12:35 -0300

    added animations to menu

--- src/js/reveal.js
+++ src/js/reveal.js
@@ -3,16 +3,32 @@ const mainright = document.querySelector('.main-right');
 const rightHero = document.querySelector('.right-hero');
 const leftHero = document.querySelector('.left-hero');
 const carrousel = document.querySelector('.carrousel');
-const navlinks = document.getElementById('main-navlinks')
+const navlinks = document.querySelector('.main-navlinks')
 const hamburger = document.querySelector('.hamburger-menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active'); 
-  if(hamburger.classList.contains('active')) {
-    navlinks.style.display = 'flex';
-  } else {
-    navlinks.style.display = 'none';
-}
+     if(hamburger.classList.contains('active')) {
+        gsap.fromTo(navlinks, {
+            filter: "blur(100px)",
+            opacity: 0,
+            y: 10,
+        }, {
+            filter: "blur(0px)",
+            opacity: 1,
+            y: 0,
+        });
+    } else {
+        gsap.fromTo(navlinks, {
+            filter: "blur(0px)",
+            opacity: 1,
+            y: 0,
+        }, {
+            filter: "blur(100px)",
+            opacity: 0,
+            y: 10,
+        });
+    }
 
 });
 
